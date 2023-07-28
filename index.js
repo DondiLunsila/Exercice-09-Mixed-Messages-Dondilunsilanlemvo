de3143dada3c6e87ec046b9e7142de10e6f486b8
dondi <dondilunsila@gmail.com>
2023-07-28 12:04:36 +0100

    fist harray

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-//Construiction d'un programme générateur de messages
+const quotes = [
+  "Believe in yourself",
+  "Dream big, achieve big",
+  "Success begins with a single step",
+  "You are capable of amazing things",
+  "Embrace the journey, not just the destination"
+];
